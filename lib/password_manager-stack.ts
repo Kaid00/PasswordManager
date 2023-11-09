caf7078c72f1534c83f66d81e6bc6e3720a43045
Azamah Jr <depixellife@gmail.com>
2023-11-09 12:13:40 +0000

    Update: provisioned lambda, apigw, dynamo

--- lib/password_manager-stack.ts
+++ lib/password_manager-stack.ts
@@ -1,16 +1,57 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
+
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PasswordManagerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    /// MARK: DEFINING THE TABLE 
+     const passwordManagerTable = new dynamodb.Table(this, 'PasswordManager', {
+      partitionKey: { name: 'UserId', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'PasswordId', type: dynamodb.AttributeType.STRING },
+      readCapacity: 5,
+      writeCapacity: 5,
+      tableName: 'PasswordManagerTable'
+     });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PasswordManagerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a global secondary index
+    passwordManagerTable.addGlobalSecondaryIndex({
+      indexName: 'WebsiteIndex',
+      partitionKey: { name: 'Website', type: dynamodb.AttributeType.STRING },
+      readCapacity: 5,
+      writeCapacity: 5,
+    });
+   
+    /// MARK: DEFINING API GATEWAY
+    const getWay = new apigateway.RestApi(this, 'PasswordManagerAPI', {});
+
+    // Gateway GET all saved passwords
+    getWay.root
+    .addResource('passwords') // to get a list of all passwords
+    .addMethod('GET')
+
+    // Add password
+    getWay.root
+    .addResource('save') // Saves a new password
+    .addMethod('POST')
+
+    /// MARK: LAMBDA CONSTRUCT
+    new lambda.Function(this, 'PasswordManagerLambda', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'passwordHandler.lambdaHandler',
+      code: lambda.Code.fromAsset('src/lambda'),
+
+    })
+  
+
+    
+  
+
   }
 }
